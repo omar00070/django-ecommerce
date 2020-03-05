fe1f3e0f9dd7ab5193d7d4c48221928dea88d4ae
Omar Salameh <omar_omarsalama@yahoo.com>
2020-03-05 01:56:32 +0100

    added a cart system that adds and removes items

--- ecommerce/core/models.py
+++ ecommerce/core/models.py
@@ -1,26 +1,62 @@
 from django.db import models
 from django.conf import settings
+from django.shortcuts import reverse
+
+
+CATEGORY_CHOICE = (
+	('S', 'Shirt'),
+	('SW','Sports Wear'),
+	('OW','Outwear')
+)
+
+LABEL_CHOICE = (
+	('P', 'primary'),
+	('S', 'secondary'),
+	('D', 'danger')
+
+)
 
 
 class Item(models.Model):
 	title = models.CharField(max_length=100)
+	description = models.TextField()
 	price = models.FloatField()
-
+	discount_price = models.FloatField(blank=True, null=True)
+	category = models.CharField(choices=CATEGORY_CHOICE, max_length=2)
+	label = models.CharField(choices=LABEL_CHOICE, max_length=2)
+	slug = models.SlugField(max_length=100)
 
 	def __str__(self):
 		return self.title
 
+	def get_absolute_url(self):
+		return reverse('core:product-detail', kwargs={
+			'slug': self.slug
+		})
+
+	def get_add_to_cart_url(self):
+		return reverse('core:add-to-cart', kwargs={
+			'slug': self.slug
+			})
+	def get_remove_from_cart_url(self):
+		return reverse('core:remove-from-cart', kwargs={
+			'slug':self.slug
+			})
 
 class OrderItem(models.Model):
+	user = models.ForeignKey(settings.AUTH_USER_MODEL,
+	on_delete=models.CASCADE) 
+	ordered = models.BooleanField(default=False)
 	item = models.ForeignKey(Item, on_delete=models.CASCADE)
+	quantity = models.IntegerField(default=1)
 
 	def __str__(self):
-		return self.item.title
+		return F'{self.quantity} of {self.item.title}'
 
 
 class Order(models.Model):
 	user = models.ForeignKey(settings.AUTH_USER_MODEL, 
-	on_delete=models.CASCADE)
+							on_delete=models.CASCADE)
 	items = models.ManyToManyField(OrderItem)
 	ordered = models.BooleanField(default=False)
 
--- ecommerce/core/views.py
+++ ecommerce/core/views.py
@@ -1,8 +1,72 @@
-from django.shortcuts import render
-from .models import Item
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Item, OrderItem, Order
+from django.views.generic import ListView, DetailView
 
-def item_list(request):
+
+def pruducts(request):
 	context = {
 		"items": Item.objects.all()
 	}
-	return render(request, 'core/item_list.html', context)
+	return render(request, 'core/home-page.html', context)
+
+
+class HomeView(ListView):
+	model = Item
+	template_name = 'core/home.html'	
+
+
+
+class ProductDetail(DetailView):
+	model = Item
+	template_name = 'core/product.html'
+
+
+
+def add_to_cart(request, slug):
+	item = get_object_or_404(Item, slug=slug)
+	order_item, created = OrderItem.objects.get_or_create(
+		item=item,
+		user=request.user,
+		ordered=False
+		)
+	order_qs = Order.objects.filter(user=request.user, ordered=False)
+	if order_qs.exists():
+		order = order_qs[0]
+		# we have to check if the item exists in the order
+		if order.items.filter(item__slug=item.slug).exists():
+			order_item.quantity += 1
+			order_item.save()
+		else:
+			order.items.add(order_item)
+	else:
+		order = Order.objects.create(user=request.user)
+		order.items.add(order_item)
+
+	return redirect('core:product-detail', slug=slug)
+
+
+def remove_from_cart(request, slug):
+	item = get_object_or_404(Item, slug=slug)
+	order_qs = Order.objects.filter(user=request.user, ordered=False)
+	if order_qs.exists():
+		order = order_qs[0]
+		# we have to check if the item exists in the order
+		if order.items.filter(item__slug=item.slug).exists():
+			order_item = OrderItem.objects.filter(
+										item=item,
+										user=request.user,
+										ordered=False
+										)[0]
+			if order_item.quantity > 1:	
+				order_item.quantity -= 1
+				order_item.save()
+			else:
+				return redirect('core:product-detail', slug=slug)
+				
+		else:
+			#message that the item is not in the order
+			pass
+	else:
+		# message that the user has no active order
+		pass
+	return redirect('core:product-detail', slug=slug)
